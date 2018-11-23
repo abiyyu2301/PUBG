5007bb56629165259d3e8b006c7321200c665d0d
unknown <taufik9c25@gmail.com>
2018-11-23 16:32:06 +0700

    quitable

--- PUBG.pl
+++ PUBG.pl
@@ -21,14 +21,13 @@ medicine(daun, 15).
 medicine(perban, 30).
 medicine(p3k, 50).
 
-deadzone(1).
+:- dynamic(enemy/4).
+:- dynamic(posisi/3).
+:- dynamic(player/6).
+:- dynamic(deadzone/1).
 
-:- dynamic enemy/3.
-:- dynamic posisi/3.
-:- dynamic player/6.
-
-initPl() :- 
-    random(1,11,Xp), random(1,11,Yp), assert(posisi(player,Xp,Yp)).
+initPl :- 
+    random(1,11,Xp), random(1,11,Yp), assertz(posisi(player,Xp,Yp)).
 
 randomW(1, X) :- X = ak47.
 randomW(2, X) :- X = pistol.
@@ -50,35 +49,35 @@ randomM(2, X) :- X = perban.
 randomM(3, X) :- X = p3k.
 
 initEn(1) :- 
-    random(1,11,Xe), random(1,11,Ye), random(1,4,We), randomW(We, S), assert(enemy(Xe,Ye,S)),
-    assert(posisi(enemy,Xe,Ye)).
+    random(1,11,Xe), random(1,11,Ye), random(1,4,We), randomW(We, S), assertz(enemy(Xe,Ye,We,S)),
+    assertz(posisi(enemy,Xe,Ye)).
 initEn(X) :- 
-    random(1,11,Xe), random(1,11,Ye), random(1,4,We), randomW(We, S), assert(enemy(Xe,Ye,S)),
-    assert(posisi(enemy,Xe,Ye)),
+    random(1,11,Xe), random(1,11,Ye), random(1,4,We), randomW(We, S), assertz(enemy(Xe,Ye,We,S)),
+    assertz(posisi(enemy,Xe,Ye)),
     Y is X -1, initEn(Y).
 
 initWe(1) :-
-    random(1,11,Xe), random(1,11,Ye), random(1,4,We), randomW(We, S), assert(posisi(S,Xe,Ye)).
+    random(1,11,Xe), random(1,11,Ye), random(1,4,We), randomW(We, S), assertz(posisi(S,Xe,Ye)).
 initWe(X) :-
-    random(1,11,Xe), random(1,11,Ye), random(1,4,We), randomW(We, S), assert(posisi(S,Xe,Ye)),
+    random(1,11,Xe), random(1,11,Ye), random(1,4,We), randomW(We, S), assertz(posisi(S,Xe,Ye)),
     Y is X - 1, initWe(Y).
 
 initAm(1) :-
-    random(1,11,Xe), random(1,11,Ye), random(1,4,Am), randomA(Am, S), assert(posisi(S,Xe,Ye)).
+    random(1,11,Xe), random(1,11,Ye), random(1,4,Am), randomA(Am, S), assertz(posisi(S,Xe,Ye)).
 initAm(X) :-
-    random(1,11,Xe), random(1,11,Ye), random(1,4,Am), randomA(Am, S), assert(posisi(S,Xe,Ye)),
+    random(1,11,Xe), random(1,11,Ye), random(1,4,Am), randomA(Am, S), assertz(posisi(S,Xe,Ye)),
     Y is X - 1, initAm(Y).
 
 initSh(1) :-
-    random(1,11,Xe), random(1,11,Ye), random(1,7,Sh), randomS(Sh, S), assert(posisi(S,Xe,Ye)).
+    random(1,11,Xe), random(1,11,Ye), random(1,7,Sh), randomS(Sh, S), assertz(posisi(S,Xe,Ye)).
 initSh(X) :-
-    random(1,11,Xe), random(1,11,Ye), random(1,4,Sh), randomS(Sh, S), assert(posisi(S,Xe,Ye)),
+    random(1,11,Xe), random(1,11,Ye), random(1,4,Sh), randomS(Sh, S), assertz(posisi(S,Xe,Ye)),
     Y is X - 1, initSh(Y).
 
 initMe(1) :-
-    random(1,11,Xe), random(1,11,Ye), random(1,4,Me), randomM(Me, S), assert(posisi(S,Xe,Ye)).
+    random(1,11,Xe), random(1,11,Ye), random(1,4,Me), randomM(Me, S), assertz(posisi(S,Xe,Ye)).
 initMe(X) :-
-    random(1,11,Xe), random(1,11,Ye), random(1,4,Me), randomM(Me, S), assert(posisi(S,Xe,Ye)),
+    random(1,11,Xe), random(1,11,Ye), random(1,4,Me), randomM(Me, S), assertz(posisi(S,Xe,Ye)),
     Y is X - 1, initMe(Y).
 
 lihat :-
@@ -104,7 +103,8 @@ tulisbaris(1) :-
     tulistitikpeta(1,10),
     nl. 
 
-tulisbaris(X) :- 
+tulisbaris(X) :-
+    X > 1,
     tulistitikpeta(X,1), 
     tulistitikpeta(X,2),
     tulistitikpeta(X,3), 
@@ -133,7 +133,6 @@ tulislihat(X,Y) :- (posisi(ak47Ammo,X,Y); posisi(pistolAmmo,X,Y); posisi(crossbo
 tulislihat(X,Y) :- posisi(player,X,Y), write(' P'),!.
 tulislihat(_,_) :- write(' _').
 
-
 input(help) :-
     write('drop     - menjatuhkan item dari inventory'), nl,
     write('take     - mengambil item dari map'), nl,
@@ -148,38 +147,40 @@ input(help) :-
     write('s        - menggerakkan pemain ke arah Selatan (bawah)'), nl,
     write('w        - menggerakkan pemain ke arah Barat (kiri)'), nl,
     write('quit     - menghentikan permainan (jangan lupa untuk menyimpan permainan)'),nl.
-input(look) :- lihat().
+
+input(look) :- lihat.
 input(map) :- tulisbaris(10).
 input(e) :- 
     posisi(player,X,Y),
     retract(posisi(player,X,Y)),
     N is Y + 1,
-    assert(posisi(player,X,N))
+    assertz(posisi(player,X,N))
     . 
 input(n) :- 
     posisi(player,X,Y),
     retract(posisi(player,X,Y)),
     E is X + 1,
-    assert(posisi(player,E,Y))
+    assertz(posisi(player,E,Y))
     . 
 input(s) :-
     posisi(player,X,Y),
     retract(posisi(player,X,Y)),
     W is X - 1,
-    assert(posisi(player,W,Y))
+    assertz(posisi(player,W,Y))
     . 
 input(w) :- 
     posisi(player,X,Y),
     retract(posisi(player,X,Y)),
     S is Y - 1,
-    assert(posisi(player,X,S))
+    assertz(posisi(player,X,S))
     . 
 
 input(start) :-
-    initPl(),
+    initPl,
     random(10, 20, E),
     initEn(E),
-    assert(player(100,0,none,0,E,0)),
+    assertz(deadzone(1)),
+    assertz(player(100,0,none,0,E,0)),
     random(10, 20, W),
     initWe(W),
     random(10, 20, M),
@@ -198,14 +199,18 @@ input(status) :-
     write('Enemy Alive      :   '), write(En), nl,
     write('Enemy Killed     :   '), write(Ki), nl.
 
-loop() :- 
+loop(quit) :-
+    write('Bye'), nl
+    . 
+
+loop(_) :- 
     read(I),
     input(I),
-    loop().
+    loop(I).
 
 
 initial :- 
     write('Selamat datang, untuk sekarang catetannya ini dulu'), nl,
     read(I),
     input(I),
-    loop().
+    loop(sembarang).
